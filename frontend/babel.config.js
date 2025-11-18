5475743ebe0f96bfb8c7abcb77ba79697476f463
Thaqif Rajab <thaqif_rajab@yahoo.com>
2025-11-18 17:07:25 +0800

    Refactor babel.config.js to simplify platform detection

--- frontend/babel.config.js
+++ frontend/babel.config.js
@@ -1,5 +1,4 @@
 module.exports = function (api) {
-  // Detect the platform via `caller`; this also configures caching based on the caller automatically.
   const platform = api.caller((caller) => caller && caller.platform);
   const isWeb = platform === 'web';
 
